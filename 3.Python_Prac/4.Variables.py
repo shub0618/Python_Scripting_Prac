5c75ccc7942d5b50610302fac1b67f3ef6ab71e6
shubham <shubhamshetkar46@gmail.com>
2024-10-25 01:41:53 +0530

    update

---
+++ 3.Python_Prac/4.Variables.py
@@ -0,0 +1,46 @@
+# Variable Assignments
+var1 = "Python"   # STRING
+var2 = 75         # Integer
+var3 = 3.5        # Float
+
+# Printing Variables
+print(var1)
+print(var2)
+print(var3)
+
+print()
+
+# Multiple assignments
+a = b = c = 65
+
+print(a)
+print(b)
+print(c)
+
+print()
+
+# Multiple Values assigned to Multiple Variables
+f, g, h, = "sample", "code", 7,
+
+# Printing Variables
+print("Variable f value is ", f)
+print("Variable g value is ", g)
+print("Variable h value is ", h)
+
+print()
+
+# Multiple Values assigned to Multiple Variables some more variables
+w, x, y, z = "alpha", "beta", 12, 5.4
+
+# Printing Variables
+print("Variable w value is ", w)
+print("Variable x value is ", x)
+print("Variable y value is ", y)
+print("Variable z value is ", z)
+
+print()
+
+print("Variable w is ", type(w))
+print("Variable x is ", type(x))
+print("Variable y is ", type(y))
+print("Variable z is ", type(z))
