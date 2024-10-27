8948f748174f758de199971ca6e058fdfa695fe2
shubham <shubhamshetkar46@gmail.com>
2024-10-28 01:05:01 +0530

    update

---
+++ 3.Python_Prac/09.Comparison_Operators.py
@@ -0,0 +1,22 @@
+#Comparison Operators
+
+a = 30
+b = 60
+
+out = (a < b)
+print(out)
+
+out = (a > b)
+print(out)
+
+out = (a == b)
+print(out)
+
+out = (a != b)
+print(out)
+
+out = (a >= b)
+print(out)
+
+out = (a <= b)
+print(out)
