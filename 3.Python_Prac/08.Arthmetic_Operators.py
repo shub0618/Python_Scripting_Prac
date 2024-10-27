d1a58a7acc1713bc9db071dfc4a1b2a78a8bf854
shubham <shubhamshetkar46@gmail.com>
2024-10-28 01:04:16 +0530

    update

---
+++ 3.Python_Prac/08.Arthmetic_Operators.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators
+# +, -, *, /, %, **.
+
+x = 2
+y = 7
+
+# Addition
+total = x + y
+print(total)
+
+# Subtraction
+total = x - y
+print(total)
+
+# Multiplication
+total = x * y
+print(total)
+
+# Division
+total = y/x
+print(total)
+
+# Modulus or getting the reminder 
+total = y % x
+print(total)
+
+# Exponential
+total = y**x
+print(total)
