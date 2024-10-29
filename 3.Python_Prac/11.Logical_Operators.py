903dcf976956a3d5ac47b701113751757eb9bcfc
shubham <shubhamshetkar46@gmail.com>
2024-10-30 00:50:36 +0530

    update

---
+++ 3.Python_Prac/11.Logical_Operators.py
@@ -0,0 +1,28 @@
+# Logical Operators
+
+# and
+# or
+
+a = 40
+b = 60
+
+x = 2
+y = 3
+
+out = (a < b) or (x > y)
+print(out)
+
+out = (a > b) or (x < y)
+print(out)
+
+out = (a > b) or (x > y)
+print(out)
+
+out = (a > b) and (x < y)
+print(out)
+
+out = (a < b) and (x < y)
+print(out)
+
+out = not(x < y)
+print(out)
