9f23e08c8cbf3cad1b73a7c87c3f54fa22a0ecb0
shubham <shubhamshetkar46@gmail.com>
2024-10-30 00:51:35 +0530

    update

---
+++ 3.Python_Prac/13.Membership_Operators.py
@@ -0,0 +1,14 @@
+# Membership Operator
+
+first_tuple = ("IOT", "DevOps", 47, 89, 1.5)
+ans = "DevOps" in first_tuple
+print(ans)
+
+ans = "DevOps" not in first_tuple
+print(ans)
+
+ans = 67 in first_tuple
+print(ans)
+
+ans = 67 not in first_tuple
+print(ans)
