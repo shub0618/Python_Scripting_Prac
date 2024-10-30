3e5f66d1134ba19c81068454f20c7cf7a02448e6
shubham <shubhamshetkar46@gmail.com>
2024-10-31 00:37:38 +0530

    update

---
+++ 3.Python_Prac/15.If_Condition.py
@@ -0,0 +1,19 @@
+# IF Condition
+x = 21
+
+if x < 30:
+    print("Inside IF block")
+    print("X is less than 30")
+
+print("Rest of the code.")
+
+x = 31
+
+if x < 30:
+    print("Inside IF block")
+    print("X is less than 30")
+
+print("Rest of the code.")
+
+
+
