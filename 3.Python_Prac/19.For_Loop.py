77986e3620e27000b62bb1a7935510341cd439a2
shubham <shubhamshetkar46@gmail.com>
2024-11-01 00:32:22 +0530

    update

---
+++ 3.Python_Prac/19.For_Loop.py
@@ -0,0 +1,22 @@
+# For Loop
+
+PLANET = "Earth"
+for i in PLANET:
+    print("Value if i is now ",i)
+
+print("Rest of the code.")
+
+print()
+
+VACCINES = ("Moderna", "Pfizer", "Sputnik v", "Covaxin", "AstraZeneca")
+
+for i in VACCINES:
+    print(f"{i} vaccine provides Immunization against covid19")
+
+print()
+
+Sample = ["Moderna", "Pfizer", "Sputnik v", "Covaxin", "AstraZeneca"]
+
+for test in Sample:
+    print(f"{test} vaccine provides Immunization against covid19")
+
